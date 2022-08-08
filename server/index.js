a65709d38feb66cd74f8218773e418c651a0fdd2
Vladson Albuquerque <vladsoncl@gmail.com>
2022-08-08 16:04:24 -0300

    token expires in 24h

--- server/index.js
+++ server/index.js
@@ -95,7 +95,7 @@ app.post('/login', (req, res) => {
                         const id = result[0].idusers
                         console.log(result[0])
                         const token = jwt.sign({ id }, "09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611", {
-                            expiresIn: '30s',
+                            expiresIn: '24h',
                         })
 
                         req.session.user = result;
